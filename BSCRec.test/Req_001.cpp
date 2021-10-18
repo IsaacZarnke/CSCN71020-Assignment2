4628a4dd9e9d5982fb29acbca347e532f2986874
Isaac Zarnke <Izarnke5568@conestogac.on.ca>
2021-10-17 20:18:07 -0400

    Created 6 new unit tests testing functions setLength() and setWidth()
    Tested 3 edge cases for each: Negative input, character input within the integer bounds and character input outside of the integer bounds.

--- BSCRec.test/Req_001.cpp
+++ BSCRec.test/Req_001.cpp
@@ -3,12 +3,14 @@
 
 extern "C" int getPerimeter(int* length, int* width);
 extern "C" int getArea(int* length, int* width);
+extern "C" void setLength(int input, int* length);
+extern "C" void setWidth(int input, int* width);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace TestingPerimeterAndArea
+namespace BCSRecTest
 {
-	TEST_CLASS(TestingPerimeterAndArea)
+	TEST_CLASS(BCSRecTest)
 	{
 	public:
 
@@ -18,7 +20,7 @@ namespace TestingPerimeterAndArea
 
 		TEST_METHOD(TestPerimeter)
 		{
-			//This is testing the getPerimeter function using 2 * 2 + 4 * 2 = 12
+			// This is testing the getPerimeter function using 2 * 2 + 4 * 2, expecting a returned value of 12
 			int result = 0;
 
 			int x = 2;
@@ -30,7 +32,7 @@ namespace TestingPerimeterAndArea
 
 		TEST_METHOD(TestArea)
 		{
-			// This is testing the getArea function using 4 * 4 = 16
+			// This is testing the getArea function using 4 * 4, expecting a returned value of 16
 			int result = 0;
 
 			int x = 4;
@@ -38,5 +40,99 @@ namespace TestingPerimeterAndArea
 			result = getArea(&x, &x);
 			Assert::AreEqual(16, result);
 		}
+
+		TEST_METHOD(TestsetLength_NegativeInput)
+		{
+			//This is testing the setlength() function for negative inputs, expecting an unchanged length value
+			int input = -5;
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+			
+
+			setLength(input, length);
+			Assert::AreEqual(1, *length);
+			delete length;
+		}
+
+		TEST_METHOD(TestsetLength_NonIntegerInputsInBounds)
+		{
+			//This is testing the setlength() function for non integer inputs within bounds of 1-100, expecting length value to equal 97
+			char input = 'a';
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+
+			setLength(input, length);
+			Assert::AreEqual(97, *length);
+			delete length;
+		}
+
+		TEST_METHOD(TestsetLength_NonIntegerInputsOutOfBounds)
+		{
+			//This is testing the setlength() function for non integer inputs within bounds of 1-100, expecting length value to be unchanged
+			char input = 'z';
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+
+			setLength(input, length);
+			Assert::AreEqual(1, *length);
+			delete length;
+		}
+
+		TEST_METHOD(TestsetWidth_NegativeInput)
+		{
+			//This is testing the setlength() function for negative inputs, expecting an unchanged length value
+			int input = -5;
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+
+
+			setLength(input, length);
+			Assert::AreEqual(1, *length);
+			delete length;
+		}
+
+		TEST_METHOD(TestsetWidth_NonIntegerInputsInBounds)
+		{
+			//This is testing the setlength() function for non integer inputs within bounds of 1-100, expecting length value to equal 97
+			char input = 'a';
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+
+			setLength(input, length);
+			Assert::AreEqual(97, *length);
+			delete length;
+		}
+
+		TEST_METHOD(TestsetWidth_NonIntegerInputsOutOfBounds)
+		{
+			//This is testing the setlength() function for non integer inputs within bounds of 1-100, expecting length value to be unchanged
+			char input = 'z';
+			int* length = new int;
+
+			*length = 1;
+
+			Assert::AreEqual(1, *length);
+
+			setLength(input, length);
+			Assert::AreEqual(1, *length);
+			delete length;
+		}
+
+
 	};
 }
